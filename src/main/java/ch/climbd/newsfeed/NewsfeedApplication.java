acccd28d300cfcbc211a1751e9e960e1960d1c6f
mithandir <mithandir@gmail.com>
2022-10-11 23:19:44 +0200

    Configure virtual thread pool for Spring Boot / Tomcat

--- src/main/java/ch/climbd/newsfeed/NewsfeedApplication.java
+++ src/main/java/ch/climbd/newsfeed/NewsfeedApplication.java
@@ -2,7 +2,14 @@ package ch.climbd.newsfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,5 +18,13 @@ public class NewsfeedApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewsfeedApplication.class, args);
     }
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
 
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
 }
